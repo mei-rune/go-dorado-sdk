bf8a12e5db5a0ee03d61bcd8f5c758d0d2c6b7c6
meifakun <runner.mei@gmail.com>
2022-07-13 19:46:08 +0800

    add UtcTime()

--- dorado/power.go
+++ dorado/power.go
@@ -5,9 +5,33 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"time"
 )
 
-func (d *Device) PowerOff(ctx context.Context, superAdminpassword string)  error {
+func (d *Device) UtcTime(ctx context.Context) (time.Time, error) {
+	spath := "/system_utc_time"
+
+	req, err := d.newRequest(ctx, "GET", spath, nil)
+	if err != nil {
+		return time.Time{}, fmt.Errorf(ErrCreateRequest+": %w", err)
+	}
+
+	var data struct {
+		CMO_SYS_UTC_TIME string
+	}
+	if err = d.requestWithRetry(req, &data, DefaultHTTPRetryCount); err != nil {
+		return time.Time{}, fmt.Errorf(ErrRequestWithRetry+": %w", err)
+	}
+
+	i64, err := strconv.ParseInt(data.CMO_SYS_UTC_TIME, 10, 64)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("CMO_SYS_UTC_TIME '%s' is invalid", data.CMO_SYS_UTC_TIME)
+	}
+	return time.Unix(i64, 0), nil
+}
+
+func (d *Device) PowerOff(ctx context.Context, superAdminpassword string) error {
 	spath := "/SYSTEM/POWEROFF"
 
 	jb, err := json.Marshal(map[string]interface{}{
@@ -29,24 +53,24 @@ func (d *Device) PowerOff(ctx context.Context, superAdminpassword string)  error
 	return nil
 }
 
-func (d *Device) Reboot(ctx context.Context, superAdminpassword string)  error {
-  spath := "/SYSTEM/REBOOT"
+func (d *Device) PowerReboot(ctx context.Context, superAdminpassword string) error {
+	spath := "/SYSTEM/REBOOT"
 
-  jb, err := json.Marshal(map[string]interface{}{
-    "IMPORTANTPSW": superAdminpassword,
-  })
-  if err != nil {
-    return fmt.Errorf(ErrCreatePostValue+": %w", err)
-  }
-  req, err := d.newRequest(ctx, "PUT", spath, bytes.NewBuffer(jb))
-  if err != nil {
-    return fmt.Errorf(ErrCreateRequest+": %w", err)
-  }
+	jb, err := json.Marshal(map[string]interface{}{
+		"IMPORTANTPSW": superAdminpassword,
+	})
+	if err != nil {
+		return fmt.Errorf(ErrCreatePostValue+": %w", err)
+	}
+	req, err := d.newRequest(ctx, "PUT", spath, bytes.NewBuffer(jb))
+	if err != nil {
+		return fmt.Errorf(ErrCreateRequest+": %w", err)
+	}
 
-  data := map[string]interface{}{}
-  if err = d.requestWithRetry(req, &data, DefaultHTTPRetryCount); err != nil {
-    return fmt.Errorf(ErrRequestWithRetry+": %w", err)
-  }
+	data := map[string]interface{}{}
+	if err = d.requestWithRetry(req, &data, DefaultHTTPRetryCount); err != nil {
+		return fmt.Errorf(ErrRequestWithRetry+": %w", err)
+	}
 
-  return nil
+	return nil
 }
